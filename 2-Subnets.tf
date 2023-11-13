5b767d649a9fc439ea025edeafc7adeaef5f211f
Blackknight05 <149324803+Blackknight05@users.noreply.github.com>
2023-11-13 12:38:44 -0500

    Update 2-Subnets.tf
    Updating region

--- 2-Subnets.tf
+++ 2-Subnets.tf
@@ -1,80 +1,80 @@
 #These are for public
 
-resource "aws_subnet" "public-eu-west-2a" {
+resource "aws_subnet" "public-me-central-1a" {
   vpc_id                  = aws_vpc.app1.id
   cidr_block              = "10.95.1.0/24"
-  availability_zone       = "eu-west-2a"
+  availability_zone       = "me-central-1a"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "public-eu-west-2a"
+    Name = "public-me-central-1a"
     Service = "application1"
     Owner = "Chewbacca"
     Planet = "Musafar"
   }
 }
 
-resource "aws_subnet" "public-eu-west-2b" {
+resource "aws_subnet" "public-me-central-1b" {
   vpc_id                  = aws_vpc.app1.id
   cidr_block              = "10.95.2.0/24"
-  availability_zone       = "eu-west-2b"
+  availability_zone       = "me-central-1b"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "public-eu-west-2b"
+    Name = "public-me-central-1b"
     Service = "application1"
     Owner = "Chewbacca"
     Planet = "Musafar"
   }
 }
 
-resource "aws_subnet" "public-eu-west-2c" {
+resource "aws_subnet" "public-me-central-1c" {
   vpc_id                  = aws_vpc.app1.id
   cidr_block              = "10.95.3.0/24"
-  availability_zone       = "eu-west-2c"
+  availability_zone       = "me-central-1c"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "public-eu-west-2c"
+    Name = "public-me-central-1c"
     Service = "application1"
     Owner = "Chewbacca"
     Planet = "Musafar"
   }
 }
 #these are for private
-resource "aws_subnet" "private-eu-west-2a" {
+resource "aws_subnet" "private-me-central-1" {
   vpc_id            = aws_vpc.app1.id
   cidr_block        = "10.95.11.0/24"
-  availability_zone = "eu-west-2a"
+  availability_zone = "me-central-1a"
 
   tags = {
-    Name = "private-eu-west-2a"
+    Name = "private-me-central-1a"
     Service = "application1"
     Owner = "Chewbacca"
     Planet = "Musafar"
   }
 }
 
-resource "aws_subnet" "private-eu-west-2b" {
+resource "aws_subnet" "private-me-central-1b" {
   vpc_id            = aws_vpc.app1.id
   cidr_block        = "10.95.12.0/24"
-  availability_zone = "eu-west-2a"
+  availability_zone = "me-central-1a"
 
   tags = {
-    Name = "private-eu-west-2b"
+    Name = "private-me-central-1b"
     Service = "application1"
     Owner = "Chewbacca"
     Planet = "Musafar"
   }
 }
 
-resource "aws_subnet" "private-eu-west-2c" {
+resource "aws_subnet" "private-me-central-1c" {
   vpc_id            = aws_vpc.app1.id
   cidr_block        = "10.95.13.0/24"
-  availability_zone = "eu-west-2c"
+  availability_zone = "me-central-1c"
 
   tags = {
-    Name = "private-eu-west-2c"
+    Name = "private-me-central-1c"
     Service = "application1"
     Owner = "Chewbacca"
     Planet = "Musafar"
